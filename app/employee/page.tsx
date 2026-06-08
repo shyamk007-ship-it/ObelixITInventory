e9eaa12b20261984ea783f19ed9fd6ab3bad0b01
Shyam kumar <SHYAM@daryaship.local>
2026-06-08 15:31:17 +0530

    latest fix

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
 import { supabase } from "../lib/supabase";
 import Sidebar from "../components/Sidebar";
 import TopBar from "../components/TopBar";
